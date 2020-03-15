d3c9a2c625f23e5b2efd6549ed824366226a7c1f
thedoritos <thedoritos@gmail.com>
2020-03-15 21:51:11 +0900

    :sparkles: Print missing files

--- packer.rb
+++ packer.rb
@@ -17,4 +17,13 @@ abort("Pass command (#{COMMANDS.join('|')}) as arg0") unless COMMANDS.include?(c
 abort("Pass environment as arg1") unless environment
 abort("Packerfile doesn't know environment: #{environment}") unless config.keys.include?(environment)
 
+files = config[environment].map { |f| File.expand_path(f) }
+
+case command
+when 'pack'
+  missing_files = files.reject { |f| File.exists?(f) }
+  abort("#{missing_files.count} files are not found:\n#{missing_files.join("\n")}") unless missing_files.empty?
+when 'unpack'
+end
+
 p config[environment]
