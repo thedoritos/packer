132853aa6c8c1b63cca1d6781431eda977d6cab4
thedoritos <thedoritos@gmail.com>
2020-03-15 23:49:50 +0900

    :sparkles: Encrypt pack

--- packer.rb
+++ packer.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 require 'yaml'
 require 'fileutils'
+require 'openssl'
 
 PACKS_DIR = "#{Dir.pwd}/Packs"
 PACKER_TMP_DIR = "#{Dir.pwd}/.packer"
@@ -28,18 +29,39 @@ when 'pack'
   missing_files = files.reject { |f| File.exists?(f) }
   abort("#{missing_files.count} files are not found:\n#{missing_files.join("\n")}") unless missing_files.empty?
 
+  print "Password: "
+  password = STDIN.gets.chomp
+
   tmp_pack = "#{PACKER_TMP_DIR}/#{Time.now.to_i}/#{environment}"
   FileUtils.rm_rf(tmp_pack)
   files.each do |f|
     dest = "#{tmp_pack}/#{f.sub(Dir.pwd, '').sub('/', '')}"
-    p dest
     FileUtils.mkdir_p(File.dirname(dest))
     FileUtils.cp(f, dest)
   end
 
   FileUtils.cd("#{tmp_pack}/..") do
     system("zip -qr #{environment}.zip *")
+
+    cipher = OpenSSL::Cipher.new('aes-256-cbc')
+    cipher.encrypt
+    key_iv = OpenSSL::PKCS5.pbkdf2_hmac(password, 'salt', 2000, cipher.key_len + cipher.iv_len, 'sha256')
+    cipher.key = key_iv[0, cipher.key_len]
+    cipher.iv = key_iv[cipher.key_len, cipher.iv_len]
+
+    File.open("#{environment}.pack", 'wb') do |output|
+      File.open("#{environment}.zip", 'rb') do |input|
+        buff = buff || ""
+        while input.read(4096, buff)
+          output << cipher.update(buff)
+        end
+        output << cipher.final
+      end
+    end
   end
+
+  FileUtils.mkdir_p(PACKS_DIR)
+  FileUtils.mv("#{tmp_pack}.pack", "#{PACKS_DIR}")
 when 'unpack'
 end
 
