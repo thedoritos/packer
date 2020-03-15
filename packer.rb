4d021c6c1d2ab4e4e92e2f54687763095e0f8573
thedoritos <thedoritos@gmail.com>
2020-03-15 21:11:45 +0900

    :sparkles: Receive command/environment

--- packer.rb
+++ packer.rb
@@ -2,10 +2,19 @@
 require 'yaml'
 
 PACKER_FILE = "#{Dir.pwd}/Packerfile"
-abort("Packer file is not found") unless File.exists?(PACKER_FILE)
+abort("Packerfile is not found") unless File.exists?(PACKER_FILE)
 
 config = YAML.load_file(PACKER_FILE)
-abort("Packer file is not a yaml") if config.inspect == "false"
+abort("Packerfile is not a yaml") if config.inspect == "false"
 
 config.each { |k, v| v.flatten! }
-p config
+
+command, environment = ARGV
+
+COMMANDS = %w(pack unpack)
+abort("Pass command (#{COMMANDS.join('|')}) as arg0") unless COMMANDS.include?(command)
+
+abort("Pass environment as arg1") unless environment
+abort("Packerfile doesn't know environment: #{environment}") unless config.keys.include?(environment)
+
+p config[environment]
