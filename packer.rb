9f77547048d714e697812242302485893ff8b712
thedoritos <thedoritos@gmail.com>
2020-03-15 22:10:34 +0900

    :sparkles: Pack to tmp dir

--- packer.rb
+++ packer.rb
@@ -1,5 +1,9 @@
 #! /usr/bin/env ruby
 require 'yaml'
+require 'fileutils'
+
+PACKS_DIR = "#{Dir.pwd}/Packs"
+PACKER_TMP_DIR = "#{Dir.pwd}/.packer"
 
 PACKER_FILE = "#{Dir.pwd}/Packerfile"
 abort("Packerfile is not found") unless File.exists?(PACKER_FILE)
@@ -23,6 +27,15 @@ case command
 when 'pack'
   missing_files = files.reject { |f| File.exists?(f) }
   abort("#{missing_files.count} files are not found:\n#{missing_files.join("\n")}") unless missing_files.empty?
+
+  tmp_pack = "#{PACKER_TMP_DIR}/#{Time.now.to_i}/#{environment}"
+  FileUtils.rm_rf(tmp_pack)
+  files.each do |f|
+    dest = "#{tmp_pack}/#{f.sub(Dir.pwd, '').sub('/', '')}"
+    p dest
+    FileUtils.mkdir_p(File.dirname(dest))
+    FileUtils.cp(f, dest)
+  end
 when 'unpack'
 end
 
